bd1cc19a7a63b8c6f71a6306ae5d426a7ba50de9
Ferrarex <104005451+Ferrarex@users.noreply.github.com>
2026-01-04 22:25:51 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const logo = document.createElement('div');
     const logoLink = document.createElement('a');
     logoLink.href = 'index.html';
-    logoLink.innerHTML = '<p><img src="images/logo-mini.png" class="logo inline-image"><span class="inline-text">Discord Armazenamento</span></p>';
+    logoLink.innerHTML = '<p><img src="images/logo.png" class="logo inline-image"><span class="inline-text">Discord Armazenamento</span></p>';
     logo.classList.add('nav-links');
     
     const navLinks = document.createElement('ul');
@@ -37,3 +37,4 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
   
+
